c64ac88e0a9d8efac467abccb121ce243da66043
Robert Smith <robert.smith160@icloud.com>
2016-08-26 14:50:32 -0600

    Updated generator

--- lib/key_gen.rb
+++ lib/key_gen.rb
@@ -1,21 +1,15 @@
 require 'pry'
 
 class KeyGen
-
+attr_reader :key
   def key_gen
-    the_array = [*0..9]
-    one_key = the_array.sample(1)
-    two_key = the_array.sample(1)
-    three_key = the_array.sample(1)
-    four_key = the_array.sample(1)
-    five_key = the_array.sample(1)
-    the_key = [[[one_key]+[two_key]+[three_key]+[four_key]+[five_key]].join.to_i]
+    initial_numbers = [*1..5]
+    key = initial_numbers.map do |num|
+      rand(9)
+    end
+    return key
   end
-  binding.pry
-
-
-
 end
 
-enigma = KeyGen.new
-binding.pry
+enigma = KeyGen.new 
+enigma.key_gen
