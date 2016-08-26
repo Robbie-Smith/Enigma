09c2187bd45eee64921112a0c79095ba07cdcdd6
Robert Smith <robert.smith160@icloud.com>
2016-08-26 16:57:58 -0600

    key_gen.rb

--- lib/key_gen.rb
+++ lib/key_gen.rb
@@ -1,15 +1,15 @@
-require 'pry'
+require 'pry'  # => true
 
 class KeyGen
-attr_reader :key
+attr_reader :key                        # => nil
   def key_gen
-    initial_numbers = [*1..5]
-    key = initial_numbers.map do |num|
-      rand(9)
-    end
-    return key
-  end
-end
+    initial_numbers = [*1..5]           # => [1, 2, 3, 4, 5]
+    key = initial_numbers.map do |num|  # => [1, 2, 3, 4, 5]
+      rand(9)                           # => 0, 8, 3, 5, 6
+    end                                 # => [0, 8, 3, 5, 6]
+    return key                          # => [0, 8, 3, 5, 6]
+  end                                   # => :key_gen
+end                                     # => :key_gen
 
-enigma = KeyGen.new 
-enigma.key_gen
+enigma = KeyGen.new  # => #<KeyGen:0x007f993b092988>
+enigma.key_gen       # => [0, 8, 3, 5, 6]
