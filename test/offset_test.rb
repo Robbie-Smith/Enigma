348b789c3f3d31d6495e39b5b3db4ddd9d20c5d3
Robert Smith <robert.smith160@icloud.com>
2016-08-26 18:47:41 -0600

    Updated the offset test

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -1,36 +1,21 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/offsets'
-require 'time'
+require './lib/offset'
 
-class OffsetsTest < Minitest::Test
+class OffsetTest < Minitest::Test
+  def test_does_time_square
+  n = Offset.new
+  n.squaring_function
 
-  def test_if_returns_position_of_date
-    time = Offsets.new
-    assert_equal 8, time.todays_date[4]
-    assert_equal 26, time.todays_date[3]
-    assert_equal 2016, time.todays_date[5]
+  assert_equal @square, n.squaring_function
   end
 
-  def test_if_can_return_date_for_offset
-    time = Offsets.new
-    assert_equal 26, time.offset_date(3)
-    assert_equal 8, time.offset_date(4)
-    assert_equal 2016, time.offset_date(5)
+  def test_offset
+    n = Offset.new
+    n.squaring_function
+    n.offset
+
+    assert_equal @offset, n.offset
   end
 
-  def test_offset_date_joined
-    time = Offsets.new
-    assert_equal '8262016', time.offset_joined
-  end
-
-  def test_offset_date_squared
-    time = Offsets.new
-    assert_equal 68260908384256, time.offset_squared
-  end
-
-  def test_last_four_for_offset
-    time = Offsets.new
-    assert_equal 4256, time.offset
-  end
 end
