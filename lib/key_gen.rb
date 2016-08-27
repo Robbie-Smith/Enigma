7ad4f9d91c28d2f55cabc72ce6ca98bae93d71af
Robert Smith <robert.smith160@icloud.com>
2016-08-26 20:19:25 -0600

    In progress key generator, working on add the key and the shift together

--- lib/key_gen.rb
+++ lib/key_gen.rb
@@ -1,15 +1,16 @@
-require 'pry'  # => true
-
+require 'pry'     # => true
+require 'matrix'  # => true
 class KeyGen
-attr_reader :key                        # => nil
+attr_reader :key  # => nil
+
   def key_gen
     initial_numbers = [*1..5]           # => [1, 2, 3, 4, 5]
     key = initial_numbers.map do |num|  # => [1, 2, 3, 4, 5]
-      rand(9)                           # => 0, 8, 3, 5, 6
-    end                                 # => [0, 8, 3, 5, 6]
-    return key                          # => [0, 8, 3, 5, 6]
+      rand(9)                           # => 5, 8, 5, 0, 1
+    end                                 # => [5, 8, 5, 0, 1]
+    return key                          # => [5, 8, 5, 0, 1]
   end                                   # => :key_gen
 end                                     # => :key_gen
 
-enigma = KeyGen.new  # => #<KeyGen:0x007f993b092988>
-enigma.key_gen       # => [0, 8, 3, 5, 6]
+enigma = KeyGen.new  # => #<KeyGen:0x007ff309840e00>
+enigma.key_gen       # => [5, 8, 5, 0, 1]
