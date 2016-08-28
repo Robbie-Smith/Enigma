4e05ddfc5cb2d6c488f19ffdf96ac5d75c76e684
Robert Smith <robert.smith160@icloud.com>
2016-08-28 15:28:08 -0600

    Updated the offset
    Refactored the offset.

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,28 +1,26 @@
-require 'pry'
+require 'pry'  # => true
 
 class Offset
-  attr_reader :square
+  attr_reader :square, :offset  # => nil
   def initialize
-    @square = ''
-    @time = Time.new
-  end
+    @square = ''                # => ""
+    @time = Time.new            # => 2016-08-28 10:06:42 -0600
+  end                           # => :initialize
 
   def squaring_function
-    if @square.nil? == false
-      date = @time.strftime("%d%m%y").to_i
-      @square = date ** 2
-    end
-    return @square
-  end
+      date = @time.strftime("%d%m%y").to_i  # => 280816
+      @square = date ** 2                   # => 78857625856
+    return @square                          # => 78857625856
+  end                                       # => :squaring_function
 
   def offset
-    square_string = @square.to_s.chars
-    square_drop = square_string.drop(7)
-    @offset = square_drop.map {|num| num.to_i}
-    return @offset
-  end
-end
+    square_string = @square.to_s.chars          # => ["7", "8", "8", "5", "7", "6", "2", "5", "8", "5", "6"]
+    square_drop = square_string.drop(7)         # => ["5", "8", "5", "6"]
+    @offset = square_drop.map {|num| num.to_i}  # => [5, 8, 5, 6]
+    return @offset                              # => [5, 8, 5, 6]
+  end                                           # => :offset
+end                                             # => :offset
 
-o = Offset.new
-o.squaring_function
-o.offset
+o = Offset.new       # => #<Offset:0x007ff6221b9200 @square="", @time=2016-08-28 10:06:42 -0600>
+o.squaring_function  # => 78857625856
+o.offset             # => [5, 8, 5, 6]
