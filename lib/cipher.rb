282a7ba27e84d952f4d59b60365e3d1fb409fc8a
Robert Smith <robert.smith160@icloud.com>
2016-08-29 16:51:23 -0600

    Update all files. Created encrypt and decrypt method that works.

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,86 +1,86 @@
-require 'pry'
-require_relative 'final_key'
+require 'pry'                 # => true
+require_relative 'final_key'  # => true
 # require './lib/offset'
 
 class Cipher
-  attr_reader :offset, :keygen, :key
-  attr_writer :key
+  attr_reader :offset, :keygen, :key  # => nil
+  attr_writer :key                    # => nil
   def initialize
-    @offset = Offset.new
-    @key = FinalKey.new
-    @final_key = @key.combine
-  end
+    @offset = Offset.new              # => #<Offset:0x007fbccb8c5ac0 @time=2016-08-29 15:14:35 -0600>
+    @key = FinalKey.new               # => #<FinalKey:0x007fbccb8c5700 @key=#<KeyGen:0x007fbccb8c56d8>, @offset=#<Offset:0x007fbccb8c56b0 @time=2016-08-29 15:14:35 -0600>, @first_key=[], @offset_holder=[], @final_key=[]>
+    @final_key = @key.combine         # => [46, 88, 42, 52]
+  end                                 # => :initialize
 
   def alpha
-    characters = ('A'..'Z').to_a
-  end
+    characters = ('A'..'Z').to_a  # => ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  end                             # => :alpha
 
   def a_value
     a_val = @final_key[0]
-  end
+  end                      # => :a_value
 
   def b_value
     a_val = @final_key[1]
-  end
+  end                      # => :b_value
 
   def c_value
     a_val = @final_key[2]
-  end
+  end                      # => :c_value
 
   def d_value
     a_val = @final_key[3]
-  end
+  end                      # => :d_value
 
   def rotate_1
-    rotated_characters = alpha.rotate(@final_key[0])
-    pairs = alpha.zip(rotated_characters).to_h
-    #Hash[pairs]
-  end
+    rotated_characters = alpha.rotate(@final_key[0])  # => ["U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"]
+    pairs = alpha.zip(rotated_characters).to_h        # => {"A"=>"U", "B"=>"V", "C"=>"W", "D"=>"X", "E"=>"Y", "F"=>"Z", "G"=>"A", "H"=>"B", "I"=>"C", "J"=>"D", "K"=>"E", "L"=>"F", "M"=>"G", "N"=>"H", "O"=>"I", "P"=>"J", "Q"=>"K", "R"=>"L", "S"=>"M", "T"=>"N", "U"=>"O", "V"=>"P", "W"=>"Q", "X"=>"R", "Y"=>"S", "Z"=>"T"}
+    # Hash[pairs]
+  end                                                 # => :rotate_1
 
   def rotate_2
     rotated_characters = alpha.rotate(@final_key[1])
     pairs = alpha.zip(rotated_characters)
     Hash[pairs]
-  end
+  end                                                 # => :rotate_2
 
   def rotate_3
     rotated_characters = alpha.rotate(@final_key[2])
     pairs = alpha.zip(rotated_characters)
     Hash[pairs]
-  end
+  end                                                 # => :rotate_3
 
   def rotate_4
     rotated_characters = alpha.rotate(@final_key[3])
     pairs = alpha.zip(rotated_characters)
     Hash[pairs]
-  end
+  end                                                 # => :rotate_4
 
   def encrypt_at_1(letter)
-    uppercase_letter = letter.upcase
-    rotate_1[uppercase_letter]
-  end
+    uppercase_letter = letter.upcase  # => "A"
+    rotate_1[uppercase_letter]        # => "U"
+  end                                 # => :encrypt_at_1
 
   def encrypt_at_2(letter)
     uppercase_letter = letter.upcase
     rotate_2[uppercase_letter]
-  end
+  end                                 # => :encrypt_at_2
 
   def encrypt_at_3(letter)
     uppercase_letter = letter.upcase
     rotate_3[uppercase_letter]
-  end
+  end                                 # => :encrypt_at_3
 
   def encrypt_at_4(letter)
     uppercase_letter = letter.upcase
     rotate_4[uppercase_letter]
-  end
+  end                                 # => :encrypt_at_4
 # binding.pry
 
-end
+end  # => :encrypt_at_4
 
-c = Cipher.new
+c = Cipher.new  # => #<Cipher:0x007fbccb8c5ae8 @offset=#<Offset:0x007fbccb8c5ac0 @time=2016-08-29 15:14:35 -0600>, @key=#<FinalKey:0x007fbccb8c5700 @key=#<KeyGen:0x007fbccb8c56d8>, @offset=#<Offset:0x007fbccb8c56b0 @time=2016-08-29 15:14:35 -0600, @date=290816, @square=84573945856, @offset=[5, 8, 5, 6]>, @first_key=[41, 80, 37, 46], @offset_holder=[5, 8, 5, 6], @final_key=[46, 88, 42, 52]>, @final_key=[46, 88, 42, 52]>
 
-c.alpha
-c.encrypt_at_1("a")
+c.alpha              # => ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+c.encrypt_at_1("a")  # => "U"
 #
 # binding.pry
--- lib/final_key.rb
+++ lib/final_key.rb
@@ -1,34 +1,33 @@
-require_relative 'key_gen'
-require_relative 'offset'
-require 'pry'
+require_relative 'key_gen'             # => true
+require_relative 'offset'              # => true
+require 'pry'                          # => false
 class FinalKey
-  attr_reader :crypted_word, :letters
+  attr_reader :crypted_word, :letters  # => nil
   def initialize
-    @key = KeyGen.new
-    @offset = Offset.new
-    @first_key = Array.new
-    @offset_holder = Array.new
-    @final_key = Array.new
-  end
+    @key = KeyGen.new                  # => #<KeyGen:0x007ffdbd19a4e8>
+    @offset = Offset.new               # => #<Offset:0x007ffdbd19a150 @time=2016-08-29 15:34:35 -0600>
+    @first_key = Array.new             # => []
+    @offset_holder = Array.new         # => []
+    @final_key = Array.new             # => []
+  end                                  # => :initialize
 
   def combine
-    @key.key_gen
-    @first_key << @key.key_gen.join[0] + @key.key_gen.join[1]
-    @first_key << @key.key_gen.join[1] + @key.key_gen.join[2]
-    @first_key << @key.key_gen.join[2] + @key.key_gen.join[3]
-    @first_key << @key.key_gen.join[3] + @key.key_gen.join[4]
-    @first_key.map! {|num| num.to_i}
-    final_key
-  end
+    @first_key << @key.key_gen.join[0] + @key.key_gen.join[1]  # => ["18"]
+    @first_key << @key.key_gen.join[1] + @key.key_gen.join[2]  # => ["18", "80"]
+    @first_key << @key.key_gen.join[2] + @key.key_gen.join[3]  # => ["18", "80", "62"]
+    @first_key << @key.key_gen.join[3] + @key.key_gen.join[4]  # => ["18", "80", "62", "82"]
+    @first_key.map! {|num| num.to_i}                           # => [18, 80, 62, 82]
+  end                                                          # => :combine
 
   def final_key
-    @offset.squaring_function
-    @offset_holder = @offset.offset
-    @final_key = @first_key.map.with_index do |value,index|
-              value + @offset_holder[index]
-            end
-    return @final_key
-  end
-end
-n = FinalKey.new
-n.combine
+    @offset.squaring_function                                # => 84573945856
+    @offset_holder = @offset.offset                          # => [5, 8, 5, 6]
+    @final_key = @first_key.map.with_index do |value,index|  # => #<Enumerator: [18, 80, 62, 82]:map>
+              value + @offset_holder[index]                  # => 23, 88, 67, 88
+            end                                              # => [23, 88, 67, 88]
+    return @final_key                                        # => [23, 88, 67, 88]
+  end                                                        # => :final_key
+end                                                          # => :final_key
+n = FinalKey.new                                             # => #<FinalKey:0x007ffdbd19a510 @key=#<KeyGen:0x007ffdbd19a4e8>, @offset=#<Offset:0x007ffdbd19a150 @time=2016-08-29 15:34:35 -0600>, @first_key=[], @offset_holder=[], @final_key=[]>
+n.combine                                                    # => [18, 80, 62, 82]
+n.final_key                                                  # => [23, 88, 67, 88]
