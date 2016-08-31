24359c6572fd9f4901f7186388684fd1c55244e0
Robert Smith <robert.smith160@icloud.com>
2016-08-30 19:40:24 -0600

    Key Gen
    Turned key to @key needed it to be an instance variable.

--- lib/key_gen.rb
+++ lib/key_gen.rb
@@ -4,13 +4,10 @@ class KeyGen
 attr_reader :key
 
   def key_gen
-    key = [*1..5]
-    key.map! do |num|
+    @key = [*1..5]
+    @key.map! do |num|
       rand(9)
     end
-    return key
+    # return @key
   end
 end
-
-enigma = KeyGen.new
-enigma.key_gen
