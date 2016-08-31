91a9b29881a15bc2b2d54856efea57d9e1ec9f21
Robert Smith <robert.smith160@icloud.com>
2016-08-30 19:39:08 -0600

    Key Gen
    Key Gen Test. 100% Coverage

--- test/key_gen_test.rb
+++ test/key_gen_test.rb
@@ -1,35 +1,24 @@
-require 'minitest/autorun'  # => true
-require 'minitest/pride'    # => true
-require './lib/key_gen'     # ~> LoadError: cannot load such file -- ./lib/key_gen
+require 'simplecov'
+SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/key_gen'
 
 class KeyGenTest < Minitest::Test
 
   def test_generate_an_array_of_numbers
     enigma = KeyGen.new
-    enigma.key_gen
     assert_instance_of Array, enigma.key_gen
   end
 
   def test_length_of_array
     enigma = KeyGen.new
-    enigma.key_gen
-
     assert_equal 5, enigma.key_gen.length
   end
 
   def test_random
-    skip
     enigma = KeyGen.new
-    enigma.key_gen
-
-    assert_instance_of Random, enigma.key_gen
+    refute_equal @key, enigma.key_gen
   end
 
 end
-
-# ~> LoadError
-# ~> cannot load such file -- ./lib/key_gen
-# ~>
-# ~> /usr/local/Cellar/ruby/2.3.1/lib/ruby/2.3.0/rubygems/core_ext/kernel_require.rb:55:in `require'
-# ~> /usr/local/Cellar/ruby/2.3.1/lib/ruby/2.3.0/rubygems/core_ext/kernel_require.rb:55:in `require'
-# ~> /Users/robertsmith/Turing/1module/Enigma/test/key_gen_test.rb:3:in `<main>'
