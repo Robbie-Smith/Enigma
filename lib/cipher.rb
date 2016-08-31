075eb0095cf4f2dd88ee2efed8ea4454829e8622
Robert Smith <robert.smith160@icloud.com>
2016-08-30 19:42:59 -0600

    Update/refactored cipher.rb

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,92 +1,91 @@
 require 'pry'
 require_relative 'final_key'
-# require './lib/offset'
 
 class Cipher
-  attr_reader :offset, :keygen, :key
-  attr_writer :key
+  attr_reader :offset, :keygen, :key, :final_key, :characters
+  attr_accessor :pairs
+
   def initialize
-    # @offset = Offset.new              # => #<Offset:0x007fcd9485d298 @time=2016-08-29 17:06:46 -0600>
     @key = FinalKey.new
     @first_key = @key.combine
     @final_key = @key.final_key
+    @pairs = pairs
   end
 
   def alpha
-    characters = ('A'..'Z').to_a
+    @characters = ('A'..'Z').to_a
   end
 
-  def a_value
-    a_val = @final_key[0]
+  def rotate_1(num_1 = 0)
+    if num_1 == 0
+      rotated_characters = alpha.rotate(@final_key[0])
+      @pairs = alpha.zip(rotated_characters).to_h
+    else
+      rotated_characters = alpha.rotate(num_1)
+      @pairs = alpha.zip(rotated_characters).to_h
+    end
+    return @pairs
   end
 
-  def b_value
-    a_val = @final_key[1]
+  def rotate_2(num_2 = 0)
+    if num_2 == 0
+      rotated_characters = alpha.rotate(@final_key[1])
+      @pairs = alpha.zip(rotated_characters).to_h
+    else
+      rotated_characters = alpha.rotate(num_2)
+      @pairs = alpha.zip(rotated_characters).to_h
+    end
   end
 
-  def c_value
-    a_val = @final_key[2]
+  def rotate_3(num_3 = 0)
+    if num_3 == 0
+      rotated_characters = alpha.rotate(@final_key[2])
+      @pairs = alpha.zip(rotated_characters).to_h
+    else
+      rotated_characters = alpha.rotate(num_3)
+      @pairs = alpha.zip(rotated_characters).to_h
+    end
   end
 
-  def d_value
-    a_val = @final_key[3]
-  end
-
-  def rotate_1
-    rotated_characters = alpha.rotate(@final_key[0])
-    pairs = alpha.zip(rotated_characters).to_h
-    # Hash[pairs]
-  end
-
-  def rotate_2
-    rotated_characters = alpha.rotate(@final_key[1])
-    pairs = alpha.zip(rotated_characters)
-    Hash[pairs]
-  end
-
-  def rotate_3
-    rotated_characters = alpha.rotate(@final_key[2])
-    pairs = alpha.zip(rotated_characters)
-    Hash[pairs]
-  end
-
-  def rotate_4
-    rotated_characters = alpha.rotate(@final_key[3])
-    pairs = alpha.zip(rotated_characters)
-    Hash[pairs]
+  def rotate_4(num_4 = 0)
+    if num_4 == 0
+      rotated_characters = alpha.rotate(@final_key[3])
+      @pairs = alpha.zip(rotated_characters).to_h
+    else
+      rotated_characters = alpha.rotate(num_4)
+      @pairs = alpha.zip(rotated_characters).to_h
+    end
   end
 
   def encrypt_at_1(letter)
-    uppercase_letter = letter.upcase
-    rotate_1[uppercase_letter]
+    @pairs[letter.upcase]
   end
 
   def encrypt_at_2(letter)
-    uppercase_letter = letter.upcase
-    rotate_2[uppercase_letter]
+    @pairs[letter.upcase]
   end
 
   def encrypt_at_3(letter)
-    uppercase_letter = letter.upcase
-    rotate_3[uppercase_letter]
+    @pairs[letter.upcase]
   end
 
   def encrypt_at_4(letter)
-    uppercase_letter = letter.upcase
-    rotate_4[uppercase_letter]
+    @pairs[letter.upcase]
   end
 
   def print_key
     @first_key
     @final_key = @key.final_key
+    return @final_key
   end
-# binding.pry
 
 end
 
-# c = Cipher.new
-#
-# c.alpha
-# c.encrypt_at_1("a")
-#
-# binding.pry
+c = Cipher.new
+
+c.alpha
+c.rotate_1
+a = c.rotate_1.fetch_values("A")
+a[0]
+c.encrypt_at_1("A")
+c.print_key
